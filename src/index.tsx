8e5d53bd5cd2b7aefeba776b7e97618fa8433272
Alex Martynov <fenixdeveloper@gmail.com>
2024-09-24 07:18:47 +0200

    Update index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -13,19 +13,25 @@ const domNode = document.getElementById('root') as HTMLDivElement;
 const root = createRoot(domNode);
 
 const App = () => {
+	const [currentArticleState, setCurrentArticleState] =
+		useState(defaultArticleState);
+	
 	return (
 		<div
 			className={clsx(styles.main)}
 			style={
 				{
-					'--font-family': defaultArticleState.fontFamilyOption.value,
-					'--font-size': defaultArticleState.fontSizeOption.value,
-					'--font-color': defaultArticleState.fontColor.value,
-					'--container-width': defaultArticleState.contentWidth.value,
-					'--bg-color': defaultArticleState.backgroundColor.value,
+					'--font-family': currentArticleState.fontFamilyOption.value,
+					'--font-size': currentArticleState.fontSizeOption.value,
+					'--font-color': currentArticleState.fontColor.value,
+					'--container-width': currentArticleState.contentWidth.value,
+					'--bg-color': currentArticleState.backgroundColor.value,
 				} as CSSProperties
 			}>
-			<ArticleParamsForm />
+			<ArticleParamsForm 
+				currentArticleState={currentArticleState}
+				setCurrentArticleState={setCurrentArticleState}	
+			/>
 			<Article />
 		</div>
 	);
