62f49e9b3fe7b71715ddaadc868938e8710ba946
Alex Martynov <fenixdeveloper@gmail.com>
2024-09-24 07:16:42 +0200

    Update ArticleParamsForm.tsx

--- src/components/article-params-form/ArticleParamsForm.tsx
+++ src/components/article-params-form/ArticleParamsForm.tsx
@@ -1,13 +1,36 @@
+import clsx from 'clsx';
+import { SyntheticEvent, useState } from 'react';
+import {
+	ArticleStateType,
+	backgroundColors,
+	contentWidthArr,
+	defaultArticleState,
+	fontColors,
+	fontFamilyOptions,
+	fontSizeOptions,
+} from 'src/constants/articleProps';
+
 import { ArrowButton } from 'components/arrow-button';
 import { Button } from 'components/button';
 
 import styles from './ArticleParamsForm.module.scss';
 
-export const ArticleParamsForm = () => {
+export const ArticleParamsForm = ({
+	currentArticleState,
+	setCurrentArticleState,
+}: {
+	currentArticleState: ArticleStateType;
+	setCurrentArticleState: (param: ArticleStateType) => void;
+}) => {
+	const [isOpen, setIsOpen] = useState(false);
+	const [state, setState = useState<ArticleStateType>(currentArticleState);
+	
 	return (
 		<>
-			<ArrowButton />
-			<aside className={styles.container}>
+			<ArrowButton onClick={() => {
+				setIsOpen(!isOpen);
+			}} />
+			<aside className={clsx(styles.container, isOpen && styles.container_open)}>>
 				<form className={styles.form}>
 					<div className={styles.bottomContainer}>
 						<Button title='Сбросить' type='reset' />
